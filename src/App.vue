5e7507d27f248ba12ff83ef4f4b36d0f0ca786dc
rioarya01 <rio283931@gmail.com>
2024-03-07 11:55:25 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <Nav></Nav>
+  <RouterView />
+</template>
+
+<script setup>
+import Nav from "@/components/Nav.vue";
+</script>
+
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1>{{ $t("home.header") }}</h1>
+    <p>{{ $t("home.created_by", {company: "Lokalise"}) }}</p>
+    <p>{{ $t("home.num_visits", 30) }}</p>
+    <p>{{ $d(new Date(), "longFormat") }}</p>
+  </main>
+</template>
+
+<script>
+// import { useI18n } from 'vue-i18n'
+// export default {
+//   setup() {
+//     const { t } = useI18n()
+//     return { t }
+//   }
+// }
+</script>
